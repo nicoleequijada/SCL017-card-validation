72c5cf1f57ece8553ce4dc57f90fdada8be442a1
Nicole Quijada <quijadabnicolea@gmail.com>
2021-05-19 20:46:52 -0400

    Agregando formulario de validacion y agregando funciones de validacion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import validator from './validator.js';
 
+let numberCredits = document.getElementById("number-credits");
+let send = document.getElementById("send");
+console.log(numberCredits);
+
+
+let getInputDate = function(event){
+    event.preventDefault()
+    let savedValue = numberCredits.value;
+    console.log(savedValue);
+    validator.isValid(savedValue);
+}
+send.addEventListener('click', getInputDate, false);
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,13 @@
 const validator = {
-  // ...
-};
+  isValid: function(creditCardNumber){
+	  let numbersArray = creditCardNumber.split(""); 
+	  console.log(numbersArray);
+		let turnedIntoNumber = parseInt(creditCardNumber);
+
+		if(creditCardNumber ===" " || isNaN(turnedIntoNumber)){
+			alert("Ingresa un numero valido");
+    }
+  }  
+}
 
 export default validator;
