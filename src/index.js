92b35c8c92e7e2882184b324c56cf5876075f23d
Nicole Quijada <quijadabnicolea@gmail.com>
2021-05-24 22:18:53 -0400

    algoritmo de luhn

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ import validator from './validator.js';
 
 let numberCredits = document.getElementById("number-credits");
 let send = document.getElementById("send");
-console.log(numberCredits);
+
 
 
 let getInputDate = function(event){
     event.preventDefault()
     let savedValue = numberCredits.value;
-    console.log(savedValue);
+    
     let result = validator.isValid(savedValue);
     if(result === false){
         alert("ingresa un numero correcto")
     }
+    
 }
 send.addEventListener('click', getInputDate, false);
 
--- src/validator.js
+++ src/validator.js
@@ -1,16 +1,30 @@
 const validator = {
   isValid: function(creditCardNumber){
 	  let numbersArray = creditCardNumber.split(""); 
-	  console.log(numbersArray);
-		let turnedIntoNumber = parseInt(creditCardNumber);
+    let turnedIntoNumber = parseInt(creditCardNumber);
 
 		if(creditCardNumber ===" " || isNaN(turnedIntoNumber) || numbersArray.length != 16){
 			return false;
     }else{
-      return true
-    }
+      for(var i = 1; i < numbersArray.length; i++){
+        if(i % 2 == 0){
+          // console.log(numbersArray[i])
+          let numberTwo = numbersArray[i]*2;
+          console.log(numberTwo);
+          if(numberTwo >= 10){
+          //  let divide = numberTwo.split("");
+          //  console.log(divide);
+            
+          }
+      
+        }
+      }
+    } 
     
-  }  
-}
+  } 
+
+} 
 
 export default validator;
+
+
